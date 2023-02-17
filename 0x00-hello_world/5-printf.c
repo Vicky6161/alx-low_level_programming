dcb0c7e1daebdb4b3744d4ae015c0439bdd0d79d
Vicky6161 <wilsonvictoria199@gmail.com>
2023-02-17 14:33:01 -0500

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,10 @@
-#include <stdio.h>
-/**
-*main - Print a line of code using puts
-*
-*Return: 0
+. #include <stdio.h>
+/
+*main - Entry point
+*Return: Always 0 (success)
 */
 int main(void)
 {
         puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+                return (0);
 }
