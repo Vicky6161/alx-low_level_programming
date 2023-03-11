dcd71323a53c6082654d4c327136609a021cf052
Vicky6161 <wilsonvictoria199@gmail.com>
2023-03-10 19:26:48 -0500

    1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -11,11 +11,9 @@
 
 int main(int argc, char *argv[])
 {
-	int i;
+(void) argv; /*Ignore argv*/
+	       printf("%d\n", argc -1);
 
-	for (i = 0; i < argc; i++)
-	{
-		printf("%s\n", argv[i]);
-	}
+
 		return (0);
 }
