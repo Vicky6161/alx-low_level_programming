6df7e795c5b9a16df7259a780bba9ffca06c04b5
Vicky6161 <wilsonvictoria199@gmail.com>
2023-04-04 08:44:29 -0400

    0-print_listint.c

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -15,8 +15,9 @@
 typedef struct listint_s
 {
 	int n;
-	struct listint_s
+	struct listint_s *next;
 } listint_t;
+
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
@@ -33,3 +34,4 @@ size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
 
+#endif
