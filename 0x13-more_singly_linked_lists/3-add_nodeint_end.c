58a5b2e61d792f65dee3c4bdb28764a675f3b19d
Vicky6161 <wilsonvictoria199@gmail.com>
2023-04-04 11:09:18 -0400

    3-ad_nodeint_end.c

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -7,7 +7,7 @@
  *
  * Return: pointer to the enw node, or NULL if it falls
  */
-listint_t "add_nodeint_end(listint_t **head, const int n)
+listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *new;
 	listint_t *temp = *head;
