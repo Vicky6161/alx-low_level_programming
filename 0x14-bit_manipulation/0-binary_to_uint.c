f553fbc062095cfc1468469c72cea08062bc5521
Vicky6161 <wilsonvictoria199@gmail.com>
2023-04-08 14:54:02 -0400

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,17 +8,21 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int i;
-	unsigned int dec_val = 0;
+	unsigned int i = 0, n = 0;
 
 	if (!b)
 		return (0);
 
-	for (i = 0; b[i]; i++)
+	while (b[i])
 	{
-		if (b[i] < '0' || b[i] > 'i')
+		if (b[i] == '0' || b[i] == '1')
+		{
+			n <<= 1;
+			n += b[i] - '0';
+			i++;
+		}
+		else
 			return (0);
-		dec_val = 2 * dec_val + (b[i] - '0');
 	}
-	return (dec_val);
+	return (n);
 }
